68fdb717e274646d38681972d615eb2660c00f86
skyrimax <MaxRoyal123@hotmail.com>
2023-02-21 23:56:14 -0500

    fix - removed the function to convert sequence as string to sequence as array from LineGenerator interface as it does not match the purpose of the interface and interfaces should not implement any function

--- LineGenerator/include/LineGenerator/ILineGenerator.hpp
+++ LineGenerator/include/LineGenerator/ILineGenerator.hpp
@@ -7,6 +7,5 @@ namespace NS
     {
     public:
         virtual std::vector<std::vector<bool>> generateLines(unsigned int nbBoxes, const std::vector<unsigned int>& lineSequence) = 0;
-        std::vector<std::vector<bool>> generateLines(unsigned int nbBoxes, const std::string& lineSequence, const std::string& separator);
     };
 }
