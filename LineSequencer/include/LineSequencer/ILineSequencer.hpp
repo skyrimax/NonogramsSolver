e4451e764defa55ff93296aa9cb017bf75f4f48e
skyrimax <MaxRoyal123@hotmail.com>
2023-03-31 00:53:05 -0400

    feat - declared interface for line sequencers

---
+++ LineSequencer/include/LineSequencer/ILineSequencer.hpp
@@ -0,0 +1,22 @@
+#ifndef ILINESEQUENCER_H
+#define ILINESEQUENCER_H
+
+#include <vector>
+
+class ILineSequencer
+{
+public:
+    using Sequence = std::vector<unsigned int>;
+    using Line = std::vector<unsigned int>;
+
+    // Constructor
+    ILineSequencer() = default;
+
+    // Destructor
+    ~ILineSequencer() = default;
+
+    // Member methods
+    virtual Sequence operator()(Line line) = 0;
+};
+
+#endif /* ILINESEQUENCER_H */
