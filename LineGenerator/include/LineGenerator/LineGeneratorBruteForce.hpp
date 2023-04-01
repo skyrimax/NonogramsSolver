4bc1652c5faf5f36415f289c2d08c748b1e9986e
skyrimax <MaxRoyal123@hotmail.com>
2023-04-01 05:27:53 -0400

    feat - brute force line generator has a line filter object it can use to filter line possibilities feat - added constructor to inject line filter into brute force line generator

--- LineGenerator/include/LineGenerator/LineGeneratorBruteForce.hpp
+++ LineGenerator/include/LineGenerator/LineGeneratorBruteForce.hpp
@@ -1,14 +1,24 @@
 #include "ILineGenerator.hpp"
 
+#include "LineFilter/ILineFilter.hpp"
+
 namespace NS
 {
     class LineGeneratorBruteForce : public ILineGenerator
     {
     public:
+        // Constructors
+        LineGeneratorBruteForce(ILineFilter& filter);
+
+        // Destructor
+        ~LineGeneratorBruteForce() = default;
+
         virtual std::vector<Line> generateLines(unsigned int nbBoxes, const Sequence& lineSequence);
 
     private:
         std::vector<Line> createAllPossibilities(unsigned int nbBoxes);
         std::vector<Line> filterPossibilities(std::vector<Line> possibilities, const Sequence& lineSequence);
+
+        ILineFilter& filter_;
     };
 }
