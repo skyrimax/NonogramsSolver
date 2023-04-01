c4eceb8acc0cb5ea4952fa09f00e59e7c8dac174
skyrimax <MaxRoyal123@hotmail.com>
2023-04-01 04:51:38 -0400

    fix - corrected the brute force line generator to use the Sequence and Line types like the rest of the classes fix - removed line possibility buffer from brute force line generator, will pass content of createAllPossibilities directly into the paramters of filterPossibilities

--- LineGenerator/include/LineGenerator/LineGeneratorBruteForce.hpp
+++ LineGenerator/include/LineGenerator/LineGeneratorBruteForce.hpp
@@ -5,12 +5,10 @@ namespace NS
     class LineGeneratorBruteForce : public ILineGenerator
     {
     public:
-        virtual std::vector<std::vector<bool>> generateLines(unsigned int nbBoxes, const std::vector<unsigned int>& lineSequence);
+        virtual std::vector<Line> generateLines(unsigned int nbBoxes, const Sequence& lineSequence);
 
     private:
         void createAllPossibilities(unsigned int nbBoxes);
         void filterPossibilities(const std::vector<unsigned int>& lineSequence);
-
-        std::vector<std::vector<bool>> _possibilities;
     };
 }
