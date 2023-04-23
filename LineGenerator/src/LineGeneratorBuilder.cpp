b424529368b6a471f3fa5cd0025059494cda2e0a
skyrimax <MaxRoyal123@hotmail.com>
2023-04-22 23:26:36 -0400

    feat - added function to create an all possible line generator as a base in the builder

--- LineGenerator/src/LineGeneratorBuilder.cpp
+++ LineGenerator/src/LineGeneratorBuilder.cpp
@@ -1,8 +1,20 @@
 #include "LineGeneratorBuilder.hpp"
 
+#include "AllPossibleLinesGenerator.hpp"
+
+#include <stdexcept>
+
 LineGeneratorBuilder::~LineGeneratorBuilder()
 {
     delete lineGenerator_;
 }
 
-LineGeneratorBuilder& LineGeneratorBuilder::allPossibleLinesGenerator()
+LineGeneratorBuilder& LineGeneratorBuilder::allPossibleLinesGenerator(unsigned int nbBoxes)
+{
+    if(lineGenerator_)
+        throw(std::logic_error("Base line generator has already been created, cannot create AllPossibleLinesGenerator"));
+    
+    lineGenerator_ = new NS::AllPossibleLinesGenerator(nbBoxes);
+
+    return *this;
+}
