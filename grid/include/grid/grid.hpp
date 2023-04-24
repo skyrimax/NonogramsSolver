bf925ae2883a4bfb66459f740080aeafeb026f62
skyrimax <MaxRoyal123@hotmail.com>
2023-04-24 00:21:20 -0400

    feat - moved row and column removal mehtods from .icc file to header file, goal is to make grid library header only

--- grid/include/grid/grid.hpp
+++ grid/include/grid/grid.hpp
@@ -476,8 +476,50 @@ public:
     // 	m_data.swap(newArray);
     // }
 
-    // void removeRows(size_type row, size_type n=1);
-    // void removeCols(size_type col, size_type n = 1);
+    // void removeRows(size_type row, size_type n=1)
+    // {
+    // 	int i;
+    // 	int j;
+
+    // 	int newNbRows = (m_nbRows - row > n) ? m_nbRows - n : row + 1;
+
+    // 	std::vector<T>newArray(newNbRows*m_nbCols);
+
+    // 	for (i = 0; i < row; ++i) {
+    // 		for (j = 0; j < m_nbCols; ++j) {
+    // 			newArray[i*m_nbCols + j] = m_data[i*m_nbCols + j];
+    // 		}
+    // 	}
+
+    // 	for (i = row + n; i < m_nbRows; ++i) {
+    // 		for (j = 0; j < m_nbCols; ++j) {
+    // 			newArray[(i - n)*m_nbCols + j] = m_data[i*m_nbCols + j];
+    // 		}
+    // 	}
+    // }
+
+    // void removeCols(size_type col, size_type n = 1)
+    // {
+    // 	int i;
+    // 	int j;
+
+    // 	int newNbCols = (m_nbCols - col > n) ? m_nbCols - n : col + 1;
+
+    // 	std::vector<T>newArray(m_nbRows*newNbCols);
+
+    // 	for (j = 0; j < col; ++j) {
+    // 		for (i = 0; i < m_nbRows; ++i) {
+    // 			newArray[i*m_nbCols + j] = m_data[i*m_nbCols + j];
+    // 		}
+    // 	}
+
+    // 	for (j = col + n; j < m_nbCols; ++j) {
+    // 		for (i = 0; i < m_nbCols; ++i) {
+    // 			newArray[i*m_nbCols + j - n] = m_data[i*m_nbCols + j];
+    // 		}
+    // 	}
+    // }
+
     void swap(Grid<T> &grid);
     void clear();
 
--- grid/src/grid.ipp
+++ grid/src/grid.ipp
@@ -5,52 +5,6 @@
 #include <exception>
 #include <utility>
 
-// template<class T>
-// void Grid<T>::removeRows(size_type row, size_type n)
-// {
-// 	int i;
-// 	int j;
-
-// 	int newNbRows = (m_nbRows - row > n) ? m_nbRows - n : row + 1;
-
-// 	std::vector<T>newArray(newNbRows*m_nbCols);
-
-// 	for (i = 0; i < row; ++i) {
-// 		for (j = 0; j < m_nbCols; ++j) {
-// 			newArray[i*m_nbCols + j] = m_data[i*m_nbCols + j];
-// 		}
-// 	}
-
-// 	for (i = row + n; i < m_nbRows; ++i) {
-// 		for (j = 0; j < m_nbCols; ++j) {
-// 			newArray[(i - n)*m_nbCols + j] = m_data[i*m_nbCols + j];
-// 		}
-// 	}
-// }
-
-// template<class T>
-// void Grid<T>::removeCols(size_type col, size_type n)
-// {
-// 	int i;
-// 	int j;
-
-// 	int newNbCols = (m_nbCols - col > n) ? m_nbCols - n : col + 1;
-
-// 	std::vector<T>newArray(m_nbRows*newNbCols);
-
-// 	for (j = 0; j < col; ++j) {
-// 		for (i = 0; i < m_nbRows; ++i) {
-// 			newArray[i*m_nbCols + j] = m_data[i*m_nbCols + j];
-// 		}
-// 	}
-
-// 	for (j = col + n; j < m_nbCols; ++j) {
-// 		for (i = 0; i < m_nbCols; ++i) {
-// 			newArray[i*m_nbCols + j - n] = m_data[i*m_nbCols + j];
-// 		}
-// 	}
-// }
-
 template<class T>
 void Grid<T>::swap(Grid<T>& grid)
 {
