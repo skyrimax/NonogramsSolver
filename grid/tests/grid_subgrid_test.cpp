018c6ee053e20de7b2d87cac6b477936812947de
skyrimax <maxroyal123@hotmail.com>
2023-05-18 18:21:02 +0200

    feat - added test to validate that elements of extracted row from a const grid are themselves const

--- grid/tests/grid_subgrid_test.cpp
+++ grid/tests/grid_subgrid_test.cpp
@@ -21,3 +21,17 @@ TEST(GridExtractRowTest, ContainsCorrectValues)
     ASSERT_EQ(grid(0, 1), 20);
     ASSERT_EQ(grid(0, 2), 30);
 }
+
+TEST(GridExtractConstRowTest, ContainsCorrectValues)
+{
+    std::vector<int> sourceVals({1, 2, 3, 4, 5, 6, 7, 8, 9});
+
+    const Grid<int> grid(3, 3, sourceVals);
+
+    auto row0 = grid.row(0);
+
+    std::is_const<const int &>::value;
+    ASSERT_TRUE(std::is_const<std::remove_reference<decltype(row0[0].get())>::type>::value);
+    ASSERT_TRUE(std::is_const<std::remove_reference<decltype(row0[1].get())>::type>::value);
+    ASSERT_TRUE(std::is_const<std::remove_reference<decltype(row0[2].get())>::type>::value);
+}
