a0a6525263f8635ec7d228216bb4639d8db2e30a
skyrimax <MaxRoyal123@hotmail.com>
2023-06-06 23:50:59 -0400

    feat - added test for method extracting multiple modifiable columns

--- grid/tests/grid_subgrid_test.cpp
+++ grid/tests/grid_subgrid_test.cpp
@@ -290,7 +290,7 @@ TEST(GridExtractRowsTest, HandleEmpty)
     EXPECT_TRUE(rows01.empty());
 }
 
-TEST(GridExtractionRowsTest, HandleNoRow)
+TEST(GridExtractRowsTest, HandleNoRow)
 {
     for(size_t j = 1; j <= 5; ++j){
         Grid<int> grid(0, j);
@@ -301,7 +301,7 @@ TEST(GridExtractionRowsTest, HandleNoRow)
     }
 }
 
-TEST(GridExtractionRowsTest, HandleNoColumn)
+TEST(GridExtractRowsTest, HandleNoColumn)
 {
     for(size_t i = 1; i < 5; ++i)
     {
@@ -313,7 +313,7 @@ TEST(GridExtractionRowsTest, HandleNoColumn)
     }
 }
 
-TEST(GridExtractionRowsTest, HandleTooManyRows)
+TEST(GridExtractRowsTest, HandleTooManyRows)
 {
     for(size_t i = 0; i < 3; ++i)
     {
@@ -323,7 +323,7 @@ TEST(GridExtractionRowsTest, HandleTooManyRows)
     }
 }
 
-TEST(GridExtractionRowsTest, HandleOutsideRange)
+TEST(GridExtractRowsTest, HandleOutsideRange)
 {
     Grid<int> grid(3, 3);
 
@@ -486,4 +486,116 @@ TEST(GridExtractConstRowsTest, ValuesNonModifiable)
     ASSERT_TRUE(std::is_const<std::remove_reference<decltype(row0(1, 0).get())>::type>::value);
     ASSERT_TRUE(std::is_const<std::remove_reference<decltype(row0(1, 1).get())>::type>::value);
     ASSERT_TRUE(std::is_const<std::remove_reference<decltype(row0(1, 2).get())>::type>::value);
+}
+
+TEST(GridExtractColsTest, HandleEmpty)
+{
+    Grid<int> grid;
+
+    auto col0 = grid.cols(0);
+
+    EXPECT_TRUE(col0.empty());
+
+    auto cols01 = grid.cols(0, 2);
+
+    EXPECT_TRUE(cols01.empty());
+}
+
+TEST(GridExtractColsTest, HandleNoRow)
+{
+    for(size_t j = 1; j <= 5; ++j){
+        Grid<int> grid(0, j);
+
+        EXPECT_TRUE(grid.cols(0).empty());
+
+        EXPECT_TRUE(grid.cols(0, 2).empty());
+    }
+}
+
+TEST(GridExtractColsTest, HandleNoColumn)
+{
+    for(size_t i = 1; i < 5; ++i)
+    {
+        Grid<int> grid(i, 0);
+
+        EXPECT_THROW(grid.cols(0), std::out_of_range);
+
+        EXPECT_THROW(grid.cols(0, 2), std::out_of_range);
+    }
+}
+
+TEST(GridExtractColsTest, HandleTooManyColumns)
+{
+    for(size_t i = 0; i < 3; ++i)
+    {
+        Grid<int> grid(3, 3);
+
+        EXPECT_THROW(grid.cols(i, 4 - i), std::out_of_range);
+    }
+}
+
+TEST(GridExtractColsTest, HandleOutsideRange)
+{
+    Grid<int> grid(3, 3);
+
+    EXPECT_THROW(grid.cols(4), std::out_of_range);
+
+    EXPECT_THROW(grid.cols(4, 2), std::out_of_range);
+}
+
+TEST(GridExtractColsTest, ContainsCorrectValues)
+{
+    std::vector<int> sourceVals({1, 2, 3, 4, 5, 6, 7, 8, 9});
+
+    Grid<int> grid(3, 3, sourceVals);
+
+    auto col0 = grid.cols(0);
+
+    EXPECT_EQ(col0(0, 0), 1);
+    EXPECT_EQ(col0(1, 0), 4);
+    EXPECT_EQ(col0(2, 0), 7);
+
+    auto cols01 = grid.cols(0, 2);
+
+    EXPECT_EQ(cols01(0, 0), 1);
+    EXPECT_EQ(cols01(0, 1), 2);
+    EXPECT_EQ(cols01(1, 0), 4);
+    EXPECT_EQ(cols01(1, 1), 5);
+    EXPECT_EQ(cols01(2, 0), 7);
+    EXPECT_EQ(cols01(2, 1), 8);
+}
+
+TEST(GridExtractColsTest, ValuesModifiable)
+{
+    std::vector<int> sourceVals({1, 2, 3, 4, 5, 6, 7, 8, 9});
+
+    Grid<int> grid1(3, 3, sourceVals);
+
+    auto col0 = grid1.cols(0);
+
+    col0(0, 0).get() = 10;
+    col0(1, 0).get() = 40;
+    col0(2, 0).get() = 70;
+
+    ASSERT_EQ(grid1(0, 0), 10);
+    ASSERT_EQ(grid1(1, 0), 40);
+    ASSERT_EQ(grid1(2, 0), 70);
+
+    Grid<int> grid2(3, 3, sourceVals);
+
+    auto cols01 = grid2.cols(0, 2);
+
+    cols01(0, 0).get() = 10;
+    cols01(0, 1).get() = 20;
+    cols01(1, 0).get() = 40;
+    cols01(1, 1).get() = 50;
+    cols01(2, 0).get() = 70;
+    cols01(2, 1).get() = 80;
+
+    ASSERT_EQ(grid2(0, 0), 10);
+    ASSERT_EQ(grid2(0, 1), 20);
+    ASSERT_EQ(grid2(1, 0), 40);
+    ASSERT_EQ(grid2(1, 1), 50);
+    ASSERT_EQ(grid2(2, 0), 70);
+    ASSERT_EQ(grid2(2, 1), 80);
 }
