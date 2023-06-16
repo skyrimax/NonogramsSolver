36f28abebda6454d9eca96f9caaaabdb36a3440d
skyrimax <MaxRoyal123@hotmail.com>
2023-06-15 23:28:03 -0400

    feat - created structure representing a nonogram

--- Solver/include/Solver/ISolver.hpp
+++ Solver/include/Solver/ISolver.hpp
@@ -1,11 +1,21 @@
 #ifndef ISOLVER_H
 #define ISOLVER_H
 
+#include <vector>
+
 namespace NS
 {
     class ISolver
     {
+    public:
+        using Sequence = std::vector<unsigned int>;
 
+        struct Nonogram
+        {
+            std::vector<Sequence> rows;
+            std::vector<Sequence> columns;
+        };
+        
     };
 }
 
