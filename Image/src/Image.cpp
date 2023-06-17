2d9e9a98778e36fba7e92464d5cddad957a25d82
skyrimax <MaxRoyal123@hotmail.com>
2023-06-16 23:20:25 -0400

    feat - implementing all constructors

---
+++ Image/src/Image.cpp
@@ -0,0 +1,21 @@
+#include "Image.hpp"
+
+NS::Image::Image()
+    : Grid<int>()
+{
+}
+
+NS::Image::Image(size_type nbRows, size_type nbCols)
+    : Grid<int>(nbRows, nbCols)
+{
+}
+
+NS::Image::Image(const Image &image)
+    : Grid<int>(image)
+{
+}
+
+NS::Image::Image(Image &&image)
+    : Grid<int>(image)
+{
+}
