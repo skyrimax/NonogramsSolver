349630f0c07ee5baaa4224dfe5508a8634d8106a
skyrimax <MaxRoyal123@hotmail.com>
2023-06-22 03:48:14 -0400

    fix - correceted constructor parameter types fix - corrected constructor parameter names feat - implemented constructors

--- NonogramLevel/src/NonogramLevel.cpp
+++ NonogramLevel/src/NonogramLevel.cpp
@@ -1,13 +1,18 @@
 #include "NonogramLevel.hpp"
 
-NS::NonogramLevel::NonogramLevel(std::vector<Sequence> rows, std::vector<Sequence> cols)
+#include <utility>
+
+NS::NonogramLevel::NonogramLevel(const std::vector<Sequence>& rows, const std::vector<Sequence>& cols)
+    : rows_(rows), cols_(cols)
 {
 }
 
-NS::NonogramLevel::NonogramLevel(const NS::NonogramLevel &image)
+NS::NonogramLevel::NonogramLevel(const NS::NonogramLevel &nonogramLevel)
+    : rows_(nonogramLevel.rows_), cols_(nonogramLevel.cols_)
 {
 }
 
-NS::NonogramLevel::NonogramLevel(NS::NonogramLevel &&image)
+NS::NonogramLevel::NonogramLevel(NS::NonogramLevel &&nonogramLevel)
+    : rows_(std::move(nonogramLevel.rows_)), cols_(std::move(nonogramLevel.cols_))
 {
 }
