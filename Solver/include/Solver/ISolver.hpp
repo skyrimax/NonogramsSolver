8e9f6f3c38cfb1fbb3386d09e1ef72919decc242
skyrimax <MaxRoyal123@hotmail.com>
2023-06-26 22:53:09 -0400

    chore - renamed nonogram level parameter of the solve method

--- Solver/include/Solver/ISolver.hpp
+++ Solver/include/Solver/ISolver.hpp
@@ -10,7 +10,7 @@ namespace NS
     class ISolver
     {
     public:
-        virtual int solve(NonogramLevel& nonogram, Image& returnImage) = 0;
+        virtual int solve(NonogramLevel& nonogramLevel, Image& returnImage) = 0;
     };
 }
 
