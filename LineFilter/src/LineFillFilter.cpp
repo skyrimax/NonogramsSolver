daecd555849ecab94d330b067956a6810201f03b
skyrimax <MaxRoyal123@hotmail.com>
2023-07-10 00:40:50 -0400

    feat - implemented comparation method of line fill filter

--- LineFilter/src/LineFillFilter.cpp
+++ LineFilter/src/LineFillFilter.cpp
@@ -1,7 +1,29 @@
 #include "LineFillFilter.hpp"
 
+#include <stdexcept>
+#include <string>
+
 LineFillFilter::LineFillFilter(LineFillFilter::Line lineToMatch)
     : lineToMatch_(lineToMatch)
 {
 
+}
+
+bool LineFillFilter::operator()(LineFillFilter::Line line) const
+{
+    auto lineSize = line.size();
+    auto lineToMatchSize = lineToMatch_.size();
+
+    if(lineSize != lineToMatchSize)
+        throw(std::logic_error(std::string("Tested line is not the same length as reference line; "
+                                            "tested line as length: ") + std::to_string(line.size()) + 
+                                            std::string(" and reference line has a length of: ") +
+                                            std::to_string(lineToMatch_.size())));
+
+    for(auto i = 0; i < lineSize; ++i){
+        if(lineToMatch_[i] == 1 && line[i] != 1)
+            return false;
+    }
+
+    return true;
 }
