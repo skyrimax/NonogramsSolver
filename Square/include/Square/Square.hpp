7517308034f93091dacd7d582095deac1d8eb306
skyrimax <MaxRoyal123@hotmail.com>
2023-08-14 05:46:54 -0400

    feat - added square class, represent all the states a square can have by adding the undefined state to the defined square class

---
+++ Square/include/Square/Square.hpp
@@ -0,0 +1,18 @@
+#ifndef SQUARE_H
+#define SQUARE_H
+
+#include "DefinedSquare.hpp"
+
+namespace NS
+{
+    class Square: public DefinedSquare
+    {
+    public:
+        enum
+        {
+            Undefined=-1
+        };
+    };
+}
+
+#endif /* SQUARE_H */
