d61c78a4ded38926c90f910981041ee337d8566b
skyrimax <MaxRoyal123@hotmail.com>
2023-08-14 05:42:28 -0400

    feat - defined a defined square class, represents the states of a square that has a defines state, either filled or empty

---
+++ Square/include/Square/DefinedSquare.hpp
@@ -0,0 +1,17 @@
+#ifndef DEFINEDSQUARE_H
+#define DEFINEDSQUARE_H
+
+namespace NS
+{
+    class DefinedSquare
+    {
+    public:
+        enum
+        {
+            Empty=0,
+            Filled=1
+        };
+    };
+}
+
+#endif /* DEFINEDSQUARE_H */
