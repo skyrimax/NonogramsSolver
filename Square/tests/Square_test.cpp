6c9f398a6c1357286e371fc34f321e72e0c4270d
skyrimax <MaxRoyal123@hotmail.com>
2023-10-09 23:16:55 -0400

    feat - added tests for equal and not equal comparation operators between Sqare and DefinedSquare

--- Square/tests/Square_test.cpp
+++ Square/tests/Square_test.cpp
@@ -1,2 +1,26 @@
 #include <gtest/gtest.h>
 
+#include <Square.hpp>
+
+TEST(SquareTest, Comparisson)
+{
+    EXPECT_EQ(NS::Square::Empty, NS::DefinedSquare::Empty);
+    EXPECT_NE(NS::Square::Empty, NS::DefinedSquare::Filled);
+
+    EXPECT_NE(NS::Square::Filled, NS::DefinedSquare::Empty);
+    EXPECT_EQ(NS::Square::Filled, NS::DefinedSquare::Filled);
+
+    EXPECT_NE(NS::Square::Undefined, NS::DefinedSquare::Empty);
+    EXPECT_NE(NS::Square::Undefined, NS::DefinedSquare::Filled);
+}
+
+TEST(DefinedSquareTest, Comparisson)
+{
+    EXPECT_EQ(NS::DefinedSquare::Empty, NS::Square::Empty);
+    EXPECT_NE(NS::DefinedSquare::Empty, NS::Square::Filled);
+    EXPECT_NE(NS::DefinedSquare::Empty, NS::Square::Undefined);
+
+    EXPECT_NE(NS::DefinedSquare::Filled, NS::Square::Empty);
+    EXPECT_EQ(NS::DefinedSquare::Filled, NS::Square::Filled);
+    EXPECT_NE(NS::DefinedSquare::Filled, NS::Square::Undefined);
+}
