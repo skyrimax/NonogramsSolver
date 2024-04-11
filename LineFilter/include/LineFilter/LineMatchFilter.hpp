a98d21fe4b63a3888db90ebbe6fd2446d491f6a9
skyrimax <maxroyal123@hotmail.com>
2024-04-11 02:03:45 -0400

    feat - the line metch filter no longer inherits from line fill filter and line empty filter, it now inherits directly from the line filter interface and implements its own filtering logic

--- LineFilter/include/LineFilter/LineMatchFilter.hpp
+++ LineFilter/include/LineFilter/LineMatchFilter.hpp
@@ -4,19 +4,22 @@
 #include "LineFillFilter.hpp"
 #include "LineEmptyFilter.hpp"
 
-class LineMatchFilter: public LineFillFilter, public LineEmptyFilter
+class LineMatchFilter: public ILineFilter
 {
 public:
     using ReferenceLine = std::vector<int>;
 
     // Constructors
-    LineMatchFilter(const ReferenceLine& lineToMatch);
+    LineMatchFilter(std::unique_ptr<const ReferenceLine> lineToMatch);
 
     // Destructor
     ~LineMatchFilter() = default;
 
     // Member methods
     virtual bool operator()(Line line) const override;
+
+protected:
+    const std::unique_ptr<const ReferenceLine> lineToMatch_;
 };
 
 #endif /* ILINEMATCHFILTER_H */
--- LineFilter/src/LineMatchFilter.cpp
+++ LineFilter/src/LineMatchFilter.cpp
@@ -3,13 +3,27 @@
 #include <stdexcept>
 #include <string>
 
-LineMatchFilter::LineMatchFilter(const ReferenceLine& lineToMatch)
-    : LineFillFilter(lineToMatch), LineEmptyFilter(lineToMatch)
+LineMatchFilter::LineMatchFilter(std::unique_ptr<const ReferenceLine> lineToMatch)
+    : lineToMatch_(std::move(lineToMatch))
 {
 
 }
 
 bool LineMatchFilter::operator()(LineMatchFilter::Line line) const
 {
-    return LineFillFilter::operator()(line) && LineEmptyFilter::operator()(line);
+    auto lineSize = line.size();
+    auto lineToMatchSize = lineToMatch_->size();
+
+    if(lineSize != lineToMatchSize)
+        throw(std::logic_error(std::string("Tested line is not the same length as reference line; "
+                                            "tested line as length: ") + std::to_string(line.size()) + 
+                                            std::string(" and reference line has a length of: ") +
+                                            std::to_string(lineToMatch_->size())));
+
+    for(auto i = 0; i < lineSize; ++i){
+        if(lineToMatch_->at(i) == line[i] || line[i] == -1)
+            return false;
+    }
+
+    return true;
 }
