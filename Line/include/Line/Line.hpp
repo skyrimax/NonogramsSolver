b00310a21d19aea82fa9eefd1b98e1b3fce69bcf
skyrimax <maxroyal123@hotmail.com>
2025-01-06 01:19:01 -0500

    feat - created the line library, containing the Line and DefinedLine classes as vectors of Square and DefinedSquare respectively

---
+++ Line/include/Line/Line.hpp
@@ -0,0 +1,13 @@
+#ifndef SEQUENCE_H
+#define SEQUENCE_H
+
+#include <vector>
+#include <Square.hpp>
+
+namespace NS
+{
+    using Line = std::vector<Square>;
+    using DefinedLine = std::vector<DefinedSquare>;
+}
+
+#endif /* SEQUENCE_H */
